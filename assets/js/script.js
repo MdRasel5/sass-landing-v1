e82aa261f1709e3dd704767d9fe71d9e2e19c461
rirumon <islamrumon420@gmail.com>
2022-07-13 17:52:24 +0600

    testimonial section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,10 @@ function closemenu(){
     menu.style.top = "-100vh";
 }
 
+// Testimonial Slider Js
+$(document).ready(function(){
+    $('.ts-slider-active').slick({
+    prevArrow: '<button class="ts-slider-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button class="ts-slider-next"><i class="fa-solid fa-chevron-right"></i></button>'
+    });
+});
