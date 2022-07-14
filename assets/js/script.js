930356167554aa75a80a74ddd7533e5a45f6d319
rirumon <islamrumon420@gmail.com>
2022-07-14 11:29:50 +0600

    pricing section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,28 @@ $(document).ready(function(){
     nextArrow: '<button class="ts-slider-next"><i class="fa-solid fa-chevron-right"></i></button>'
     });
 });
+
+// Pricing Tabel Js
+const button = document.getElementById("selector-bar");
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+    var monthPayment = document.getElementsByTagName("h4");
+    var annualPayment = document.getElementsByTagName("h5");
+    var i;
+    var n;
+
+    for (i = 0; i < monthPayment.length; i++) {
+        for (n = 0; n < annualPayment.length; n++) {
+            if (monthPayment[i].style.display === "none") {
+                monthPayment[i].style.display = "block";
+                annualPayment[i].style.display = "none";
+            } else {
+                monthPayment[i].style.display = "none";
+                annualPayment[i].style.display = "block";
+            }
+        }
+    }
+
+
+}
